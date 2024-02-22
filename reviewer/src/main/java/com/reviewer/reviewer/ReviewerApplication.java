aabb6bdbd4506d2b3a504b58dc44cc81e9e75f3e
mob7ca <Byanka.Monteiro@br.bosch.com>
2024-02-22 08:26:18 -0300

    feat: project base

---
+++ reviewer/src/main/java/com/reviewer/reviewer/ReviewerApplication.java
@@ -0,0 +1,13 @@
+package com.reviewer.reviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewerApplication.class, args);
+	}
+
+}
