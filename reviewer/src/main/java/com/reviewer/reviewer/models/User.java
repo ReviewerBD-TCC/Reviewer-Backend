6c27038412af8953cca865c7f4122db2964e79bf
mob7ca <Byanka.Monteiro@br.bosch.com>
2024-02-26 10:29:32 -0300

    feat: model user created

--- reviewer/src/main/java/com/reviewer/reviewer/models/User.java
+++ reviewer/src/main/java/com/reviewer/reviewer/models/User.java
@@ -1,2 +1,69 @@
-package com.reviewer.reviewer.models;public class User {
+package com.reviewer.reviewer.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String user;
+    private String type;
+    private String gkz;
+    private String manager;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        String role = "ROLE_USER";
+
+        if ("admin".equals(getType())){
+            role = "ROLE_ADMIN";
+        }
+
+        return List.of(new SimpleGrantedAuthority(role));
+
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
