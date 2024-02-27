8381036a100b79c0f709589b74613103d4c6c880
mob7ca <Byanka.Monteiro@br.bosch.com>
2024-02-27 10:31:17 -0300

    feat: Password encryption created

--- reviewer/src/main/java/com/reviewer/reviewer/infra/security/SecurityConfigurations.java
+++ reviewer/src/main/java/com/reviewer/reviewer/infra/security/SecurityConfigurations.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -39,6 +40,9 @@ public class SecurityConfigurations {
         return configuration.getAuthenticationManager();
     }
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 }
