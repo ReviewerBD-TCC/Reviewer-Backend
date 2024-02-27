e8646f9d87a7f8792efd2fd84a9bb46c2862f176
hora-keven <kevensantos993@gmail.com>
2024-02-27 16:44:06 -0300

    feat: adding user service to find by email

--- reviewer/src/main/java/com/reviewer/reviewer/services/UserService.java
+++ reviewer/src/main/java/com/reviewer/reviewer/services/UserService.java
@@ -1,7 +1,21 @@
 package com.reviewer.reviewer.services;
 
 
+import com.reviewer.reviewer.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-public class UserService {
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByEmail(email);
+    }
 }
