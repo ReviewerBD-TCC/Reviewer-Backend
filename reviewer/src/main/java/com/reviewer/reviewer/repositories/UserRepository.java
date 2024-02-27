b660b814bfb9e98502c0d88ff706b9ca3f861094
mob7ca <Byanka.Monteiro@br.bosch.com>
2024-02-27 10:34:53 -0300

    feat: "findByLogin" method added to UserRepository

--- reviewer/src/main/java/com/reviewer/reviewer/repositories/UserRepository.java
+++ reviewer/src/main/java/com/reviewer/reviewer/repositories/UserRepository.java
@@ -1,2 +1,7 @@
-package com.reviewer.reviewer.repositories;public interface UserRepository {
+package com.reviewer.reviewer.repositories;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository {
+    UserDetails findByLogin(String login);
 }
