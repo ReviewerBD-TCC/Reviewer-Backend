9e34848bb322aae2ef8ff7dff5303be6f04bd7b2
hora-keven <ct67ca@bosch.com>
2024-02-27 14:30:24 -0300

    Commit suas alterações locais

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/AuthenticationController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/AuthenticationController.java
@@ -6,7 +6,7 @@ import com.reviewer.reviewer.dto.users.RegisterDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.token.TokenService;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
