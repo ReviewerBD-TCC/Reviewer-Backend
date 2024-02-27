123d0c7d5d911201c43fb966be12121e97d202f3
mob7ca <Byanka.Monteiro@br.bosch.com>
2024-02-27 14:07:59 -0300

    feat: Adding constructor

--- reviewer/src/main/java/com/reviewer/reviewer/dto/users/RegisterResponse.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/users/RegisterResponse.java
@@ -1,4 +1,9 @@
 package com.reviewer.reviewer.dto.users;
 
-public record RegisterResponse (long id, String name, String email,  String user, String type, String gkz, String manager){
+import com.reviewer.reviewer.models.User;
+
+public record RegisterResponse (long id, String name, String email, String user, String type, String gkz, String manager){
+    public RegisterResponse(User register) {
+        this(register.getId(), register.getName(), register.getEmail(), register.getUser(), register.getType(), register.getGkz(), register.getManager());
+    }
 }
