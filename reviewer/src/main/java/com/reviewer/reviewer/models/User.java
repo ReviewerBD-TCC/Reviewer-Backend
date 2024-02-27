70bdf6a87779c38835f401f56d586962e87cf13a
hora-keven <kevensantos993@gmail.com>
2024-02-27 16:45:47 -0300

    feat: change the name of table

--- reviewer/src/main/java/com/reviewer/reviewer/models/User.java
+++ reviewer/src/main/java/com/reviewer/reviewer/models/User.java
@@ -10,8 +10,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-@Table(name = "usuarios")
-@Entity(name = "Usuario")
+@Table(name = "users")
+@Entity(name = "user")
 @Getter
 @Setter
 @NoArgsConstructor
