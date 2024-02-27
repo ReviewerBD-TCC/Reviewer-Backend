3eb5d853e7da1e26c305f7b256ec655b1ceae222
hora-keven <kevensantos993@gmail.com>
2024-02-27 16:49:05 -0300

    feat: Adding atribute id

--- reviewer/src/main/java/com/reviewer/reviewer/dto/users/RegisterDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/users/RegisterDto.java
@@ -1,4 +1,4 @@
 package com.reviewer.reviewer.dto.users;
 
-public record RegisterDto (String name, String email, String password, String user, String type, String gkz, String manager ){
+public record RegisterDto (Long id, String name, String email, String password, String user, String type, String gkz, String manager ){
 }
