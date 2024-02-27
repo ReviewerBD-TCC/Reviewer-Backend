a95f4cb7ea827cf890ef010a65ca0044f1d1e6a0
hora-keven <kevensantos993@gmail.com>
2024-02-27 16:45:15 -0300

    feat: editing variable of findByEmail method

--- reviewer/src/main/java/com/reviewer/reviewer/repositories/UserRepository.java
+++ reviewer/src/main/java/com/reviewer/reviewer/repositories/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    UserDetails findByLogin(String login);
+    UserDetails findByEmail(String email);
 }
