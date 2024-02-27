f336492e6c808847b7c1d97e289bf89833347fc4
hora-keven <kevensantos993@gmail.com>
2024-02-27 16:50:12 -0300

    feat: Fixing some wrongs of methods

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/AuthenticationController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/AuthenticationController.java
@@ -10,26 +10,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
-@RequestMapping(name = "auth")
+@RequestMapping("/auth")
 public class AuthenticationController {
+
     @Autowired
     private TokenService tokenService;
 
-    @PostMapping(name = "/login")
-    public ResponseEntity<?> login(@RequestBody LoginDto loginDto){
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginDto loginDto){
         return ResponseEntity.status(HttpStatus.OK).body(new LoginResponseDTO(tokenService.login(loginDto)));
     }
 
-    @PostMapping(name = "/register")
-    public ResponseEntity<?> register(@RequestBody RegisterDto registerDto){
-        return ResponseEntity.status(HttpStatus.CREATED).body(new RegisterResponse(tokenService.register(registerDto)));
+    @PostMapping("/register")
+    @Transactional
+    public ResponseEntity<RegisterResponse> register(@RequestBody @Valid RegisterDto registerDto, UriComponentsBuilder uriBuilder){
+        var uri = uriBuilder.path("/register/{id}").buildAndExpand(registerDto.user()).toUri();
+        return ResponseEntity.created(uri).body(new RegisterResponse(tokenService.register(registerDto)));
     }
-
-
 }
