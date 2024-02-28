5c450a3f8449c81135f75101d0c691997ee371b8
hora-keven <kevensantos993@gmail.com>
2024-02-28 16:27:18 -0300

    feat: questionException and userException added

---
+++ reviewer/src/main/java/com/reviewer/reviewer/infra/exceptions/QuestionException.java
@@ -0,0 +1,8 @@
+package com.reviewer.reviewer.infra.exceptions;
+
+public class QuestionException extends RuntimeException{
+    public QuestionException(String message){
+        super(message);
+    }
+    
+}
