d55157a2324d14a22b236afae1b9104de36e905e
byankatomaz <byankatomaz@gmail.com>
2024-03-04 08:37:54 -0300

    fix: Conflicts

--- reviewer/src/main/java/com/reviewer/reviewer/models/QuestionAnswer.java
+++ reviewer/src/main/java/com/reviewer/reviewer/models/QuestionAnswer.java
@@ -1,24 +1,17 @@
 package com.reviewer.reviewer.models;
 
-<<<<<<< HEAD
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-=======
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.reviewer.reviewer.dto.questions.QuestionAnswerDto;
 import jakarta.persistence.*;
->>>>>>> origin/keven
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.Set;
 
 @Entity(name = "question_answer")
 @Table(name = "questions_answer")
@@ -27,12 +20,9 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class  QuestionAnswer{
-<<<<<<< HEAD
 
-=======
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
->>>>>>> origin/keven
     private Long id;
     @OneToOne(cascade = CascadeType.DETACH)
     @JoinColumn(name = "user_id")
