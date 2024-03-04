e9034b2c8045bf28f3ced4ddce2a96a96c8240f1
byankatomaz <byankatomaz@gmail.com>
2024-03-04 13:38:03 -0300

    Feat: Add constructor

--- reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionDto.java
@@ -1,4 +1,9 @@
 package com.reviewer.reviewer.dto.questions;
 
+import com.reviewer.reviewer.models.Question;
+
 public record QuestionDto(String question, Boolean active) {
+    public QuestionDto(Question question) {
+        this(question.getQuestion(), question.getActive());
+    }
 }
