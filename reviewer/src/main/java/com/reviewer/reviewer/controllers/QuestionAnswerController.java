7c8710742e57378b46c61715d36efda6522ce8b4
byankatomaz <byankatomaz@gmail.com>
2024-03-04 08:38:59 -0300

    fix: Endpoint

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/QuestionAnswerController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/QuestionAnswerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("answer_form")
+@RequestMapping("/answer_form")
 public class QuestionAnswerController {
     @Autowired
     private QuestionAnswerService service;
