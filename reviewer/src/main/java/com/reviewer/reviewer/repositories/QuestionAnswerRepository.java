215c5222372997d3fc91d09a7d22343d3d797c13
byankatomaz <byankatomaz@gmail.com>
2024-03-04 08:37:40 -0300

    fix: Conflicts

--- reviewer/src/main/java/com/reviewer/reviewer/repositories/QuestionAnswerRepository.java
+++ reviewer/src/main/java/com/reviewer/reviewer/repositories/QuestionAnswerRepository.java
@@ -2,12 +2,6 @@ package com.reviewer.reviewer.repositories;
 
 import com.reviewer.reviewer.models.QuestionAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 
-=======
-import org.springframework.stereotype.Repository;
-
-@Repository
->>>>>>> origin/keven
 public interface QuestionAnswerRepository extends JpaRepository<QuestionAnswer, Long> {
 }
