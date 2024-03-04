1e0261c6870916736c0c0eed5c014936e4e5a2bc
byankatomaz <byankatomaz@gmail.com>
2024-03-04 08:36:35 -0300

    fix: Conflicts

--- reviewer/src/main/java/com/reviewer/reviewer/repositories/QuestionFormRepository.java
+++ reviewer/src/main/java/com/reviewer/reviewer/repositories/QuestionFormRepository.java
@@ -2,12 +2,6 @@ package com.reviewer.reviewer.repositories;
 
 import com.reviewer.reviewer.models.QuestionForm;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 
-=======
-import org.springframework.stereotype.Repository;
-
-@Repository
->>>>>>> origin/keven
 public interface QuestionFormRepository extends JpaRepository<QuestionForm, Long> {
 }
