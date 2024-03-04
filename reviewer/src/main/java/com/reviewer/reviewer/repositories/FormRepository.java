72a42dba112df568d620024ecedd91842af8ccfd
byankatomaz <byankatomaz@gmail.com>
2024-03-04 08:38:36 -0300

    fix: Conflicts

--- reviewer/src/main/java/com/reviewer/reviewer/repositories/FormRepository.java
+++ reviewer/src/main/java/com/reviewer/reviewer/repositories/FormRepository.java
@@ -2,12 +2,6 @@ package com.reviewer.reviewer.repositories;
 
 import com.reviewer.reviewer.models.Form;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 
-=======
-import org.springframework.stereotype.Repository;
-
-@Repository
->>>>>>> origin/keven
 public interface FormRepository extends JpaRepository<Form, Long> {
 }
