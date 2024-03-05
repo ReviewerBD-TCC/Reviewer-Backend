5cdf765a3b6d25fbb28e1668ef41d0c5a1d1e9d8
hora-keven <kevensantos993@gmail.com>
2024-03-05 10:16:21 -0300

    feat: Form controller create with hypur endpoints

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/FormController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/FormController.java
@@ -1,8 +1,11 @@
 package com.reviewer.reviewer.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +15,11 @@ import com.reviewer.reviewer.dto.forms.FormQuestionDto;
 import com.reviewer.reviewer.dto.forms.FormsDto;
 import com.reviewer.reviewer.services.FormService;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 @RestController
 @RequestMapping("form")
+@SecurityRequirement(name = "bearer-key")
 public class FormController {
 
     @Autowired
@@ -29,5 +35,9 @@ public class FormController {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.createFormQuestion(formId, questionId));
 
     }
-    
+       @GetMapping("/{formId}")
+    public ResponseEntity<List<FormQuestionDto>> listFormQuestion(@PathVariable(name = "formId") Long formId){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.listFormQuestion(formId));
+
+    }
 }
