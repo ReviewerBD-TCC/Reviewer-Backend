38717895bd8a3a2f6fe4bcf6f092c22000d947d6
hora-keven <kevensantos993@gmail.com>
2024-03-05 11:14:45 -0300

    fix: bugs imports

--- reviewer/src/main/java/com/reviewer/reviewer/infra/springdoc/SpringDocConfigurations.java
+++ reviewer/src/main/java/com/reviewer/reviewer/infra/springdoc/SpringDocConfigurations.java
@@ -7,9 +7,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 import io.swagger.v3.oas.models.info.License;
 
