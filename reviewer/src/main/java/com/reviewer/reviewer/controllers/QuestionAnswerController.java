2e78743ccd241767c3ffc935a503a062d459bda5
hora-keven <kevensantos993@gmail.com>
2024-03-05 13:55:12 -0300

    feat: controllers questionAnswer e authentication

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/QuestionAnswerController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/QuestionAnswerController.java
@@ -7,9 +7,14 @@ import com.reviewer.reviewer.services.QuestionAnswerService;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +30,18 @@ public class QuestionAnswerController {
     @PostMapping
     public ResponseEntity<QuestionAnswerResponseDto> create(@RequestBody @Valid QuestionAnswerDto data){
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(data));
+    }
+    @GetMapping
+    public ResponseEntity<List<QuestionAnswerResponseDto>> findAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<QuestionAnswerResponseDto> findById(@PathVariable(name = "id")Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+    }
+    @GetMapping("/user/{id}")
+    public ResponseEntity<List<QuestionAnswerResponseDto>> findByUserId(@PathVariable(name = "id")Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(service.findByUserId(id));
     }
 
 }
