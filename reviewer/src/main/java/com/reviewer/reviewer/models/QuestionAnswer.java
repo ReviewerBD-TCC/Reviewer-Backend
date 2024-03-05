7904b9e42f542bb48ab012b82ddb6451c043e92e
byankatomaz <byankatomaz@gmail.com>
2024-03-05 16:55:23 -0300

    fix: Add constructor

--- reviewer/src/main/java/com/reviewer/reviewer/models/QuestionAnswer.java
+++ reviewer/src/main/java/com/reviewer/reviewer/models/QuestionAnswer.java
@@ -1,19 +1,13 @@
 package com.reviewer.reviewer.models;
 
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import java.util.List;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.reviewer.reviewer.dto.questions.QuestionAnswerDto;
 
-import jakarta.persistence.*;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,29 +20,20 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class  QuestionAnswer{
 
+public class  QuestionAnswer{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
-	private User user;
+    private User user;
 
     @ManyToOne
     @JoinColumn(name = "question_id")
     private QuestionForm questionForm;
- 
+
     private String answer;
 
-    public QuestionAnswer(User user, QuestionForm questionForm, String answer){
-        this.user = user;
-        this.questionForm = questionForm;
-        this.answer = answer;
-    }
-
-   
 }
-
