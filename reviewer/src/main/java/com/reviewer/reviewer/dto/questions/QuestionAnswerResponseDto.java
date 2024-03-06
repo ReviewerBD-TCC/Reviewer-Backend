cd2a491f0adb61efaba6dbe02547849677336264
byankatomaz <byankatomaz@gmail.com>
2024-03-06 16:55:14 -0300

    fix: Changing constructor

--- reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionAnswerResponseDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionAnswerResponseDto.java
@@ -8,7 +8,7 @@ import com.reviewer.reviewer.models.QuestionAnswer;
 
 public record QuestionAnswerResponseDto(Long id, Long userId, String name, String question, String answer) {
     public QuestionAnswerResponseDto(QuestionAnswer data){
-        this(data.getId(), data.getUser().getId(), data.getUser().getName(), data.getQuestionForm().getQuestion().getQuestion(), data.getAnswer());
+        this(data.getId(), data.getUser().getId(), data.getUser().getName(), data.getFormQuestion().getQuestion().getQuestion(), data.getAnswer());
     }
     
 }
