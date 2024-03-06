b48b2de01d3da8fa480c748d55f36322e279c63e
byankatomaz <byankatomaz@gmail.com>
2024-03-06 16:54:33 -0300

    fix: Add annotation transactional

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/QuestionController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/QuestionController.java
@@ -7,6 +7,7 @@ import com.reviewer.reviewer.dto.questions.QuestionDetailsDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,7 @@ public class QuestionController {
     private QuestionService service;
 
     @PostMapping
+    @Transactional
     public ResponseEntity<QuestionDetailsDto> create(@RequestBody @Valid QuestionDto data){
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(data));
     }
