6c6ee84c3b41d5b3672a34111a0ff24f1aca6d9f
byankatomaz <byankatomaz@gmail.com>
2024-03-11 15:14:52 -0300

    feat: indication form response dto updated

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormIndicationResponseDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormIndicationResponseDto.java
@@ -2,12 +2,18 @@ package com.reviewer.reviewer.dto.forms;
 
 import com.reviewer.reviewer.models.FormIndication;
 
+import java.util.List;
+
 public record FormIndicationResponseDto(
         Long id,
         Long indicatingUser,
-        IndicatedUserDto indicatedUsers
+        List<IndicatedUserDto> indicatedUsers
 ) {
-    public FormIndicationResponseDto(FormIndication formIndicated, IndicatedUserDto indicated) {
-        this(formIndicated.getId(), formIndicated.getIndicatingUser().getId(), indicated);
+//    public FormIndicationResponseDto(FormIndication formIndicated, IndicatedUserDto indicated) {
+//        this(formIndicated.getId(), formIndicated.getIndicatingUser().getId(), indicated);
+//    }
+
+    public FormIndicationResponseDto(FormIndicationDto form) {
+        this(form.id(), form.indicatingUser(), form.indicatedUsers());
     }
 }
