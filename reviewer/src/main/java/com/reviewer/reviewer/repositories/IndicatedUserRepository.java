31207c4c804539d870c7b97872ace1518d3cf7d6
byankatomaz <byankatomaz@gmail.com>
2024-03-19 16:48:52 -0300

    ci: Deleted files

--- reviewer/src/main/java/com/reviewer/reviewer/repositories/IndicatedUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.reviewer.reviewer.repositories;
-
-import com.reviewer.reviewer.models.IndicatedUsers;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IndicatedUserRepository extends JpaRepository<IndicatedUsers, Long> {
-}
