510bb909c2920672c4e8109923abfa824346eb8c
byankatomaz <byankatomaz@gmail.com>
2024-03-19 16:53:14 -0300

    fix: Arranging dto from referral

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormIndicationDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormIndicationDto.java
@@ -1,18 +1,10 @@
 package com.reviewer.reviewer.dto.forms;
 
-import com.reviewer.reviewer.models.IndicatedUsers;
-import com.reviewer.reviewer.models.User;
-import jakarta.validation.Valid;
-
 import java.util.List;
 
-public record
-FormIndicationDto(
+public record FormIndicationDto(
         Long id,
-        Long indicatingUser,
-        List<IndicatedUserDto> indicatedUsers
+        Long userIndication,
+        List<IndicatedDto> indicados
 ) {
-//    public FormIndicationDto(FormIndicationResponseDto formIndicated) {
-//        this(formIndicated.id(), formIndicated.indicatingUser(), formIndicated.indicatedUsers());
-//    }
 }
