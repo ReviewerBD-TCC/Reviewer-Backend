c8f1b09f26a3e12cd65d7d2b33ebd10592afab7b
byankatomaz <byankatomaz@gmail.com>
2024-03-19 16:53:42 -0300

    fix: Arranging controler from referral

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/FormIndicationController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/FormIndicationController.java
@@ -2,9 +2,6 @@ package com.reviewer.reviewer.controllers;
 
 import com.reviewer.reviewer.dto.forms.*;
 import com.reviewer.reviewer.models.FormIndication;
-import com.reviewer.reviewer.models.IndicatedUsers;
-import com.reviewer.reviewer.repositories.FormIndicationRepository;
-import com.reviewer.reviewer.repositories.IndicatedUserRepository;
 import com.reviewer.reviewer.repositories.UserRepository;
 import com.reviewer.reviewer.services.FormIndicationService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -26,49 +23,21 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class FormIndicationController {
 
     @Autowired
-    private FormIndicationRepository repository;
-
-    @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private IndicatedUserRepository indicatedUserRepository;
 
     @Resource
     private FormIndicationService formIndicationService;
 
-    @PostMapping("/indication")
+    @PostMapping
     @Transactional
-    public ResponseEntity<IndicandoResponseDto> create(@RequestBody @Valid IndicandoDto dados, UriComponentsBuilder uriBuilder){
+    public ResponseEntity<FormIndicationResponseDto> create(@RequestBody @Valid FormIndicationDto data, UriComponentsBuilder uriBuilder){
 
-        var usuarioIndicando = userRepository.findById(dados.indicando());
+        var userIndication = userRepository.findById(data.userIndication());
+        var formIndication = new FormIndication(data.id(), userIndication.get());
 
-//
-        var formulario = new FormIndication(dados.id(), usuarioIndicando.get());
-//
-//        repository.save(formIndicated);
-//
-        var uri = uriBuilder.path("/form_indication/{id}").buildAndExpand(formulario.getId()).toUri();
+        var uri = uriBuilder.path("/form_indication/{id}").buildAndExpand(formIndication.getId()).toUri();
 
-
-
-        return ResponseEntity.created(uri).body(formIndicationService.create(dados));
+        return ResponseEntity.created(uri).body(formIndicationService.create(data));
     }
-
-//    @PostMapping("/indicated_user")
-//    @Transactional
-//    public ResponseEntity<IndicatedUserResponseDto> create(@RequestBody @Valid IndicatedUserDto dados, UriComponentsBuilder uriBuilder){
-//
-//        var user = userRepository.findById(dados.user_id());
-//
-//        var indicatedUser = new IndicatedUsers(dados.id(), user.get());
-//
-//        indicatedUserRepository.save(indicatedUser);
-//
-//        var uri = uriBuilder.path("/indicated_user/{id}").buildAndExpand(indicatedUser.getId()).toUri();
-//
-//        return ResponseEntity.created(uri).body(new IndicatedUserResponseDto(indicatedUser));
-//    }
-
 
 }
