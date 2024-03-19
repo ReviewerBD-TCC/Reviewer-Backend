64cd8e1bc642c36811405afa0e747ea37a22c778
byankatomaz <byankatomaz@gmail.com>
2024-03-19 16:58:38 -0300

    feat: Creating error handler class

---
+++ reviewer/src/main/java/com/reviewer/reviewer/infra/exceptions/ErrorHandler.java
@@ -0,0 +1,20 @@
+package com.reviewer.reviewer.infra.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+}
+
