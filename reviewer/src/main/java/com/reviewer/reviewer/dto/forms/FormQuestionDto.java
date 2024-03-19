319c61b704f933a52bfa02e49e82834cb098f0a6
byankatomaz <byankatomaz@gmail.com>
2024-03-19 16:57:47 -0300

    fix: Fixing formQuestion dto

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormQuestionDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormQuestionDto.java
@@ -1,8 +1,13 @@
 package com.reviewer.reviewer.dto.forms;
 
+import com.reviewer.reviewer.models.FormQuestion;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 public record FormQuestionDto(Long formId, String question, LocalDate year) {
-} 
+    public FormQuestionDto(FormQuestion formQuestion) {
+        this(formQuestion.getId(), formQuestion.getQuestion().getQuestion(), formQuestion.getForm().getYear());
+    }
+}
     
 
