b5bb614311a94621adb246b2742d8f56d983c79f
byankatomaz <byankatomaz@gmail.com>
2024-03-21 09:26:54 -0300

    fix: Add annotations notnull

--- reviewer/src/main/java/com/reviewer/reviewer/dto/users/RegisterDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/users/RegisterDto.java
@@ -1,4 +1,29 @@
 package com.reviewer.reviewer.dto.users;
 
-public record RegisterDto (Long id, String name, String email, String password, String user, String type, String gkz, String manager ){
+import com.reviewer.reviewer.dto.users.Enums.TypeRole;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RegisterDto (
+
+        @NotBlank
+        String name,
+
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String password,
+
+        @NotBlank
+        String user,
+
+        @NotNull
+        TypeRole type,
+
+        @NotBlank
+        String gkz,
+
+        @NotBlank
+        String manager ){
 }
