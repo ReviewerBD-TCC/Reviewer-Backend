ec7f9593b78cf82d5d114c5f080dce06203131ac
byankatomaz <byankatomaz@gmail.com>
2024-03-21 09:32:21 -0300

    fix: Added annotations

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormsDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormsDto.java
@@ -5,10 +5,16 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.reviewer.reviewer.dto.questions.QuestionDto;
+import jakarta.validation.constraints.NotNull;
 
 
+public record FormsDto(
 
+        @NotNull
+        LocalDate year,
 
-public record FormsDto(Long id, LocalDate year,  LocalDate validation) {
+        @NotNull
+        LocalDate validation
+) {
 
 }
