c1d8c58306d8601034bb50b3798b344c6ae3ba18
byankatomaz <byankatomaz@gmail.com>
2024-03-21 09:24:00 -0300

    fix: Type long

--- reviewer/src/main/java/com/reviewer/reviewer/dto/users/RegisterResponseDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/users/RegisterResponseDto.java
@@ -1,8 +1,9 @@
 package com.reviewer.reviewer.dto.users;
 
+import com.reviewer.reviewer.dto.users.Enums.TypeRole;
 import com.reviewer.reviewer.models.User;
 
-public record RegisterResponseDto (long id, String name, String email, String user, String type, String gkz, String manager){
+public record RegisterResponseDto (Long id, String name, String email, String user, TypeRole type, String gkz, String manager){
     public RegisterResponseDto(User register) {
         this(register.getId(), register.getName(), register.getEmail(), register.getUser(), register.getType(), register.getGkz(), register.getManager());
     }
