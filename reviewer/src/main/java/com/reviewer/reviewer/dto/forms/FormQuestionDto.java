12426c1a3f914fe1d51f544d81ec0485c8e4b4ad
byankatomaz <byankatomaz@gmail.com>
2024-03-21 10:10:03 -0300

    fix: Added annotations

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormQuestionDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormQuestionDto.java
@@ -1,12 +1,21 @@
 package com.reviewer.reviewer.dto.forms;
 
 import com.reviewer.reviewer.models.FormQuestion;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-public record FormQuestionDto(Long formId, String question, LocalDate year) {
+public record FormQuestionDto(
+
+        @NotBlank
+        String question,
+
+        @NotNull
+        LocalDate year
+) {
     public FormQuestionDto(FormQuestion formQuestion) {
-        this(formQuestion.getId(), formQuestion.getQuestion().getQuestion(), formQuestion.getForm().getYear());
+        this(formQuestion.getQuestion().getQuestion(), formQuestion.getForm().getYear());
     }
 }
     
