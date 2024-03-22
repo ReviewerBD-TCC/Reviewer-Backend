78bae98bb07b4e07c498eb8498c2e2cc7184ace0
hora-keven <kevensantos993@gmail.com>
2024-03-22 11:16:00 -0300

    feat: form question dto, updated

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormQuestionDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormQuestionDto.java
@@ -1,22 +1,19 @@
 package com.reviewer.reviewer.dto.forms;
 
+import com.reviewer.reviewer.dto.questions.QuestionDto;
+import com.reviewer.reviewer.dto.questions.QuestionsByIdDto;
+import com.reviewer.reviewer.models.Form;
 import com.reviewer.reviewer.models.FormQuestion;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-public record FormQuestionDto(
+import java.util.List;
 
-        @NotBlank
-        String question,
-
-        @NotNull
-        LocalDate year
-) {
-    public FormQuestionDto(FormQuestion formQuestion) {
-        this(formQuestion.getQuestion().getQuestion(), formQuestion.getForm().getYear());
+public record FormQuestionDto(Long id, @NotBlank List<QuestionsByIdDto> questions, @NotNull LocalDate year) {
+    public FormQuestionDto(List<QuestionsByIdDto> questions, FormQuestion formQuestion) {
+        this(formQuestion.getForm().getId(),questions, formQuestion.getForm().getYear());
     }
+   
 }
-    
 
