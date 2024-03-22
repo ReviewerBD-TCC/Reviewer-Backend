80b4802c57c80880b4120719da881ed44a0374bc
hora-keven <kevensantos993@gmail.com>
2024-03-22 11:15:33 -0300

    feat: cross origin in form indication controller

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/FormIndicationController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/FormIndicationController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RestController
 @RequestMapping("/form_indication")
 @SecurityRequirement(name = "bearer-key")
+@CrossOrigin
 public class FormIndicationController {
 
     @Autowired
