5a88b18313e27c4b8dff13b5fb939bf0a96cb11e
hora-keven <kevensantos993@gmail.com>
2024-03-25 14:15:38 -0300

    feat: clean code import

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormsDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/FormsDto.java
@@ -1,12 +1,6 @@
 package com.reviewer.reviewer.dto.forms;
-
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-
-import com.reviewer.reviewer.dto.questions.QuestionDto;
 import jakarta.validation.constraints.NotNull;
-
 
 public record FormsDto(
 
