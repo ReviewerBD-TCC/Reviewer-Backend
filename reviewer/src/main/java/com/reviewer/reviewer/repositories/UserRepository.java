c8b0be5005ffb4c1117452e2052263858b5fa901
hora-keven <kevensantos993@gmail.com>
2024-03-25 14:16:37 -0300

    feat: clean code import

--- reviewer/src/main/java/com/reviewer/reviewer/repositories/UserRepository.java
+++ reviewer/src/main/java/com/reviewer/reviewer/repositories/UserRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     UserDetails findByEmail(String email);
-    UserDetails findByName(String name);
+
 }
