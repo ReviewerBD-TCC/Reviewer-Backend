2016cf21ab74372edcb1ffb7e4b8c006a9627a0a
hora-keven <kevensantos993@gmail.com>
2024-03-26 11:13:05 -0300

    feat:question answer response dto created

--- reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionAnswerResponseDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionAnswerResponseDto.java
@@ -2,9 +2,10 @@ package com.reviewer.reviewer.dto.questions;
 import com.reviewer.reviewer.models.QuestionAnswer;
 
 
-public record QuestionAnswerResponseDto(Long id, Long userId, String name, String question, String answer) {
+
+public record QuestionAnswerResponseDto(Long id, Long userId, String name, String questionPt,String questionEn, String answer) {
     public QuestionAnswerResponseDto(QuestionAnswer data){
-        this(data.getId(), data.getUser().getId(), data.getUser().getName(), data.getFormQuestion().getQuestion().getQuestion(), data.getAnswer());
+        this(data.getId(), data.getUser().getId(), data.getUser().getName(), data.getFormQuestion().getQuestion().getQuestionPt(),data.getFormQuestion().getQuestion().getQuestionPt(), data.getAnswer());
     }
     
 }
