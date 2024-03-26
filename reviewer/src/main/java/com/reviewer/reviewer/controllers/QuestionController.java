30d7959512aa8b67ced2eb1dc06ace07fc7ad234
hora-keven <kevensantos993@gmail.com>
2024-03-26 10:57:44 -0300

    feat: slash taked out question controller

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/QuestionController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/QuestionController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +25,8 @@ import jakarta.validation.Valid;
 
 @RestController
 @SecurityRequirement(name = "bearer-key")
-@RequestMapping("/question")
+@CrossOrigin
+@RequestMapping("question")
 public class QuestionController {
 
     @Autowired
