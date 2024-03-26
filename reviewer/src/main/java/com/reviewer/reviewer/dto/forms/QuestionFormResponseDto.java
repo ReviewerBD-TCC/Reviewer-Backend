5c906743adf5c163fc6799f1524cc251b7ff2aa0
hora-keven <kevensantos993@gmail.com>
2024-03-26 11:06:22 -0300

    feat: question form response dto created

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormResponseDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormResponseDto.java
@@ -1,12 +1,14 @@
 package com.reviewer.reviewer.dto.forms;
 
+
 import java.time.LocalDate;
 
-import com.reviewer.reviewer.models.FormQuestion;
+import com.reviewer.reviewer.models.QuestionForm;
 
-public record QuestionFormResponseDto(Long id, String questions, LocalDate year) {
+
+public record QuestionFormResponseDto(Long id, String questionPt, String quentionEn, LocalDate year) {
     
-    public QuestionFormResponseDto(FormQuestion data){
-        this(data.getForm().getId(), data.getQuestion().getQuestion(), data.getForm().getYear());
+    public QuestionFormResponseDto(QuestionForm data){
+        this(data.getForm().getId(), data.getQuestion().getQuestionPt(), data.getQuestion().getQuestionEn(),data.getForm().getYear());
     }
 }
