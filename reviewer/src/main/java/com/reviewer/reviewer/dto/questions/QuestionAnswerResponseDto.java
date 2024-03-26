7b017b18db2f210c6bb16a7cd4205aba55ffaaf7
hora-keven <kevensantos993@gmail.com>
2024-03-26 11:22:20 -0300

    feat: get the question form data entered into each related model

--- reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionAnswerResponseDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionAnswerResponseDto.java
@@ -5,7 +5,7 @@ import com.reviewer.reviewer.models.QuestionAnswer;
 
 public record QuestionAnswerResponseDto(Long id, Long userId, String name, String questionPt,String questionEn, String answer) {
     public QuestionAnswerResponseDto(QuestionAnswer data){
-        this(data.getId(), data.getUser().getId(), data.getUser().getName(), data.getFormQuestion().getQuestion().getQuestionPt(),data.getFormQuestion().getQuestion().getQuestionPt(), data.getAnswer());
+        this(data.getId(), data.getUser().getId(), data.getUser().getName(), data.getQuestionForm().getQuestion().getQuestionPt(),data.getQuestionForm().getQuestion().getQuestionPt(), data.getAnswer());
     }
     
 }
