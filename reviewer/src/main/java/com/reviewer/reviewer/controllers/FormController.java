522ee4f320395e464af44a6b871eb102723536a8
hora-keven <kevensantos993@gmail.com>
2024-03-26 10:29:05 -0300

    feat: annotation cross origin added in application

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/FormController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/FormController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,8 +21,9 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/form")
+@CrossOrigin
 @SecurityRequirement(name = "bearer-key")
+@RequestMapping("form")
 public class FormController {
 
     @Autowired
