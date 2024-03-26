c1a32208f5bfa876c54cc7a85099a61a5ea4e00e
hora-keven <kevensantos993@gmail.com>
2024-03-26 10:58:46 -0300

    feat: cross origin added in users

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/UserController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/UserController.java
@@ -2,17 +2,24 @@ package com.reviewer.reviewer.controllers;
 
 import com.reviewer.reviewer.dto.users.UserResponseDto;
 import com.reviewer.reviewer.services.UserService;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
+@CrossOrigin(origins = "*")
+@SecurityRequirement(name="bearer_key")
 @RestController
-@RequestMapping("/user")
+@RequestMapping("users")
 public class UserController {
 
     @Autowired
