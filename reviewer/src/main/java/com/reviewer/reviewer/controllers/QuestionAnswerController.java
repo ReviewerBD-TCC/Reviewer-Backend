7ba5bd9115d48ef1891dfd1575c29f56ac50423d
byankatomaz <byankatomaz@gmail.com>
2024-03-27 08:50:47 -0300

    Fix: Added role access

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/QuestionAnswerController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/QuestionAnswerController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,18 +32,22 @@ public class QuestionAnswerController {
 
     @PostMapping
     @Transactional
+    @Secured("ROLE_USER")
     public ResponseEntity<QuestionAnswerResponseDto> create(@RequestBody @Valid QuestionAnswerDto data){
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(data));
     }
     @GetMapping
+    @Secured("ROLE_ADMIN")
     public ResponseEntity<List<QuestionAnswerResponseDto>> findAll(){
         return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
     }
     @GetMapping("/{id}")
+    @Secured("ROLE_ADMIN")
     public ResponseEntity<QuestionAnswerResponseDto> findById(@PathVariable(name = "id")Long id){
         return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
     }
     @GetMapping("/user/{id}")
+    @Secured("ROLE_ADMIN")
     public ResponseEntity<List<QuestionAnswerResponseDto>> findByUserId(@PathVariable(name = "id")Long id){
         return ResponseEntity.status(HttpStatus.OK).body(service.findByUserId(id));
     }
