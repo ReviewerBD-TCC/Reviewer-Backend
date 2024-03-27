3e833e5aa4f2f545e02748c446a162a96bfca64e
byankatomaz <byankatomaz@gmail.com>
2024-03-27 08:51:04 -0300

    Fix: Added role access

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/FormIndicationController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/FormIndicationController.java
@@ -1,13 +1,14 @@
 package com.reviewer.reviewer.controllers;
 import com.reviewer.reviewer.dto.forms.*;
-import com.reviewer.reviewer.models.FormIndication;
+import com.reviewer.reviewer.models.IndicationForm;
 import com.reviewer.reviewer.repositories.UserRepository;
-import com.reviewer.reviewer.services.FormIndicationService;
+import com.reviewer.reviewer.services.IndicationFormService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,16 +28,17 @@ public class FormIndicationController {
     private UserRepository userRepository;
 
     @Resource
-    private FormIndicationService formIndicationService;
+    private IndicationFormService formIndicationService;
 
     @PostMapping
     @Transactional
+    @Secured("ROLE_USER")
     public ResponseEntity<FormIndicationResponseDto> create(@RequestBody @Valid FormIndicationDto data, UriComponentsBuilder uriBuilder){
 
         var userIndication = userRepository.findById(data.userIndication());
-        var formIndication = new FormIndication(userIndication.get());
+        var indicationForm = new IndicationForm(userIndication.get());
 
-        var uri = uriBuilder.path("/form_indication/{id}").buildAndExpand(formIndication.getId()).toUri();
+        var uri = uriBuilder.path("/form_indication/{id}").buildAndExpand(indicationForm.getId()).toUri();
 
         return ResponseEntity.created(uri).body(formIndicationService.create(data));
     }
