8f1d4f5667460ad075c3201c6b9f19f91e48ac41
byankatomaz <byankatomaz@gmail.com>
2024-03-27 08:49:35 -0300

    Fix: Added role access

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/UserController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/UserController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,7 @@ public class UserController {
     private UserService service;
 
     @GetMapping
+    @Secured("ROLE_ADMIN")
     public ResponseEntity<List<UserResponseDto>> findAll(){
         return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
     }
