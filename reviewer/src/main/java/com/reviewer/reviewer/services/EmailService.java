58552a2bf1d419a456379cc0ffa65192c5609a1e
hora-keven <kevensantos993@gmail.com>
2024-03-27 16:04:43 -0300

    feat: email servoce created

---
+++ reviewer/src/main/java/com/reviewer/reviewer/services/EmailService.java
@@ -0,0 +1,59 @@
+package com.reviewer.reviewer.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import com.reviewer.reviewer.dto.email.EmailRecordDto;
+import com.reviewer.reviewer.dto.email.EmailResponseDto;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	@Value("${spring.mail.email}")
+	private String sender;
+	
+		
+	public EmailResponseDto sendMail(EmailRecordDto data) {
+		try {
+			MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+			MimeMessageHelper mimeMessageHelper;
+			mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+			mimeMessageHelper.setFrom(sender);
+			mimeMessageHelper.setTo(data.to());
+			mimeMessageHelper.setText(data.body(), true);
+			mimeMessageHelper.setSubject(data.subject());
+			mimeMessageHelper.setCc(data.cc());
+			
+			var file = data.attchment();
+			if(file != null) {
+				for(int i = 0; i < data.attchment().length; i++) {
+					mimeMessageHelper.addAttachment(file[i].getOriginalFilename(), new ByteArrayResource(file[i].getBytes()));
+				}
+			}
+			
+			
+			javaMailSender.send(mimeMessage);
+			
+			return new EmailResponseDto("Email send successfully!");
+		}
+		
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+	
+		
+	}
+	
+
+}
