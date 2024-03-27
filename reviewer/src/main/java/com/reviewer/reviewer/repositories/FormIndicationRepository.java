51482733dc61d552b814942397778254edaacb67
byankatomaz <byankatomaz@gmail.com>
2024-03-27 08:58:33 -0300

    del

--- reviewer/src/main/java/com/reviewer/reviewer/repositories/FormIndicationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.reviewer.reviewer.repositories;
-
-import com.reviewer.reviewer.models.FormIndication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FormIndicationRepository extends JpaRepository<FormIndication, Long> {
-}
