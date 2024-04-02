bbc0207db6df21b2f62f047710af23593c8f9c06
byankatomaz <byankatomaz@gmail.com>
2024-04-02 08:07:43 -0300

    feat: Added endpoint me in controller

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/UserController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.reviewer.reviewer.controllers;
 
+import com.reviewer.reviewer.dto.users.UserDetailsResponseDto;
 import com.reviewer.reviewer.dto.users.UserResponseDto;
 import com.reviewer.reviewer.services.UserService;
 
@@ -9,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 
@@ -30,6 +33,16 @@ public class UserController {
     @Secured("ROLE_ADMIN")
     public ResponseEntity<List<UserResponseDto>> findAll(){
         return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+    }
+
+    @GetMapping("/me")
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
+    public ResponseEntity<UserDetailsResponseDto> getCurrentUser(Principal principal) {
+        if (principal instanceof UsernamePasswordAuthenticationToken) {
+            return ResponseEntity.status(HttpStatus.OK).body(service.findMe(principal));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 
 }
