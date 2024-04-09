94447ccb0f06db7889d83e5d0dc3c62eec59d380
Hora-keven <keven@gmail.com>
2024-04-09 16:16:56 -0300

    feat: String year created, to the client send the request with the year

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormListDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormListDto.java
@@ -1,9 +1,8 @@
 package com.reviewer.reviewer.dto.forms;
-import java.time.LocalDate;
 import java.util.List;
 
 
 
-public record QuestionFormListDto(String title, LocalDate year, List<Long>questionsId) {
+public record QuestionFormListDto(String title, String year, List<Long>questionsId) {
     
 }
