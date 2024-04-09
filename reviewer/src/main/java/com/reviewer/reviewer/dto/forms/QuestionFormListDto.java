68b0e18da091aa184c92835294414111fb80b828
hora-keven <keven@gmail.com>
2024-04-09 12:43:45 -0300

    feat: title, year added in question form list dto

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormListDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormListDto.java
@@ -1,8 +1,9 @@
 package com.reviewer.reviewer.dto.forms;
+import java.time.LocalDate;
 import java.util.List;
 
 
 
-public record QuestionFormListDto(List<Long>questionsId) {
+public record QuestionFormListDto(String title, LocalDate year, List<Long>questionsId) {
     
 }
