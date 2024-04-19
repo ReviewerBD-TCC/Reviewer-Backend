d0c4eb51e41fe41ec9b924941e3848c3b1ae573b
hora-keven <kevensantos993@gmail.com>
2024-04-19 10:35:41 -0300

    fix: activity not finished, but not yet

--- reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionAnswerResponseDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionAnswerResponseDto.java
@@ -1,11 +1,15 @@
 package com.reviewer.reviewer.dto.questions;
+import java.util.List;
+
 import com.reviewer.reviewer.models.QuestionAnswer;
 
 
 
-public record QuestionAnswerResponseDto(Long id, Long userId, String name, String questionPt,String questionEn, String answer) {
-    public QuestionAnswerResponseDto(QuestionAnswer data){
-        this(data.getId(), data.getUser().getId(), data.getUser().getName(), data.getQuestionForm().getQuestion().getQuestionPt(),data.getQuestionForm().getQuestion().getQuestionPt(), data.getAnswer());
+public record QuestionAnswerResponseDto(Long id, Long userId, String name, List<QuestionResponseDto> questions, String [] answers) {
+    public QuestionAnswerResponseDto(QuestionAnswer data, List<QuestionResponseDto> questions, String [] answers){
+        this(data.getId(), data.getUser().getId(), data.getUser().getName(), questions, answers);
     }
+
+    
     
 }
