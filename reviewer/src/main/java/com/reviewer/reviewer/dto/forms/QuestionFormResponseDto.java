9de9f0214e68536b4dcbc8f3d04861096f8deb0d
hora-keven <kevensantos993@gmail.com>
2024-04-22 16:46:40 -0300

    feat: list incremented to show question in format of list

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormResponseDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormResponseDto.java
@@ -2,13 +2,15 @@ package com.reviewer.reviewer.dto.forms;
 
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.reviewer.reviewer.dto.questions.QuestionResponseDto;
 import com.reviewer.reviewer.models.QuestionForm;
 
 
-public record QuestionFormResponseDto(Long id, String questionPt, String quentionEn, LocalDate year) {
+public record QuestionFormResponseDto(Long id, String title, List<QuestionResponseDto> questions, LocalDate year) {
     
-    public QuestionFormResponseDto(QuestionForm data){
-        this(data.getForm().getId(), data.getQuestion().getQuestionPt(), data.getQuestion().getQuestionEn(),data.getForm().getYear());
+    public QuestionFormResponseDto(QuestionForm data, List<QuestionResponseDto> questions){
+        this(data.getForm().getId(), data.getForm().getTitle(), questions,data.getForm().getYear());
     }
 }
