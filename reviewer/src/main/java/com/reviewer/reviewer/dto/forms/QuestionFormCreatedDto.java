e88f48bf0d6f98960afd68b5154a5c8716ed998c
byankatomaz <byankatomaz@gmail.com>
2024-04-24 14:33:13 -0300

    fix: question form response

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormCreatedDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormCreatedDto.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 
-public record QuestionFormCreatedDto(Long id, String title, @NotBlank List<QuestionsByIdDto> questions, @NotNull LocalDate year) {
+public record QuestionFormCreatedDto(Long id, String title, @NotBlank List<QuestionsByIdDto> questions, @NotNull int year) {
     public QuestionFormCreatedDto(List<QuestionsByIdDto> questions, QuestionForm formQuestion) {
         this(formQuestion.getForm().getId(), formQuestion.getForm().getTitle(), questions, formQuestion.getForm().getYear());
     }
