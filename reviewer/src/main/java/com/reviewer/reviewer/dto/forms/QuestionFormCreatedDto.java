e5edeae7b1017ded2c79c8d95cab8b7205cf8e83
hora-keven <kevensantos993@gmail.com>
2024-04-24 13:10:49 -0300

    fix:  questions by id Dto removed.

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormCreatedDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormCreatedDto.java
@@ -1,5 +1,5 @@
 package com.reviewer.reviewer.dto.forms;
-import com.reviewer.reviewer.dto.questions.QuestionsByIdDto;
+import com.reviewer.reviewer.dto.questions.QuestionResponseDto;
 import com.reviewer.reviewer.models.QuestionForm;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -7,8 +7,8 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 
-public record QuestionFormCreatedDto(Long id, String title, @NotBlank List<QuestionsByIdDto> questions, @NotNull LocalDate year) {
-    public QuestionFormCreatedDto(List<QuestionsByIdDto> questions, QuestionForm formQuestion) {
+public record QuestionFormCreatedDto(Long id, String title, @NotBlank List<QuestionResponseDto> questions, @NotNull LocalDate year) {
+    public QuestionFormCreatedDto(List<QuestionResponseDto> questions, QuestionForm formQuestion) {
         this(formQuestion.getForm().getId(), formQuestion.getForm().getTitle(), questions, formQuestion.getForm().getYear());
     }
    
