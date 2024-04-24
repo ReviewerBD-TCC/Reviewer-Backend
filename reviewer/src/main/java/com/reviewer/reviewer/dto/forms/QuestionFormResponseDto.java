d7fd6fc520f5cd4df80cec6259f7a1456d2cf821
byankatomaz <byankatomaz@gmail.com>
2024-04-24 14:32:54 -0300

    fix: response

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormResponseDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormResponseDto.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 import com.reviewer.reviewer.models.QuestionForm;
 
 
-public record QuestionFormResponseDto(Long id, String questionPt, String quentionEn, LocalDate year) {
+public record QuestionFormResponseDto(Long id, String questionPt, String quentionEn, int year) {
     
     public QuestionFormResponseDto(QuestionForm data){
         this(data.getForm().getId(), data.getQuestion().getQuestionPt(), data.getQuestion().getQuestionEn(),data.getForm().getYear());
