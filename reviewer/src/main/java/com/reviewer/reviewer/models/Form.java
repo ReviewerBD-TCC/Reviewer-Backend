25e15dc82e08ffab55b252baf56d50e7306ee240
byankatomaz <byankatomaz@gmail.com>
2024-04-24 14:33:39 -0300

    fix: year response

--- reviewer/src/main/java/com/reviewer/reviewer/models/Form.java
+++ reviewer/src/main/java/com/reviewer/reviewer/models/Form.java
@@ -28,10 +28,10 @@ public class Form {
     private String title;
 
     @DateTimeFormat(pattern = "yyyy")
-    private LocalDate year;
+    private int year;
 
     @DateTimeFormat(pattern = "yyyy")
-    private LocalDate validation;
+    private int validation;
 
     @OneToMany(mappedBy = "form", cascade = CascadeType.ALL)
     private List<QuestionForm> questionForms;
