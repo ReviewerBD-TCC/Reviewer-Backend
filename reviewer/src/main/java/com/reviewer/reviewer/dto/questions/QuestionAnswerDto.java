b90f990cbfec2de56bd5156df729d63848278168
hora-keven <kevensantos993@gmail.com>
2024-04-24 13:11:20 -0300

    feat: now its possible insert a list of answers

--- reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionAnswerDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionAnswerDto.java
@@ -1,7 +1,4 @@
 package com.reviewer.reviewer.dto.questions;
-
-
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record QuestionAnswerDto (
