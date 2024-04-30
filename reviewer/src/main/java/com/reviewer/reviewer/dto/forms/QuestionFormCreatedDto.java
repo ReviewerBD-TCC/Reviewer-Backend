f92893268ddc558d451626cdaa9d4a6f244739d9
hora-keven <kevensantos993@gmail.com>
2024-04-30 13:09:22 -0300

    feat: best practice using local date

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormCreatedDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormCreatedDto.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 
-public record QuestionFormCreatedDto(Long id, String title, @NotBlank List<QuestionResponseDto> questions, @NotNull int year) {
+public record QuestionFormCreatedDto(Long id, String title, @NotBlank List<QuestionResponseDto> questions, @NotNull LocalDate year) {
     public QuestionFormCreatedDto(List<QuestionResponseDto> questions, QuestionForm formQuestion) {
         this(formQuestion.getForm().getId(), formQuestion.getForm().getTitle(), questions, formQuestion.getForm().getYear());
     }
