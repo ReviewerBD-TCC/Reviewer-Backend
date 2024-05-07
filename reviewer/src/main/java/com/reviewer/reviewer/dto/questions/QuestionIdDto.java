90ab93cc54127db21bffc1cab9967653a77a9d46
byankatomaz <byankatomaz@gmail.com>
2024-05-07 13:12:48 -0300

    fix: dto

--- reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionIdDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionIdDto.java
@@ -2,7 +2,7 @@ package com.reviewer.reviewer.dto.questions;
 
 import jakarta.validation.constraints.NotNull;
 
-public record QuestionIdDto(
+public record QuestionIdDto (
         @NotNull
         Long question
 ) {
