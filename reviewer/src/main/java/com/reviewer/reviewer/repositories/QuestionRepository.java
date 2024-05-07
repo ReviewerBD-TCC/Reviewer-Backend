b1ca1e926bb632862807b362d906f65885fe5797
byankatomaz <byankatomaz@gmail.com>
2024-05-07 13:12:33 -0300

    fix: import

--- reviewer/src/main/java/com/reviewer/reviewer/repositories/QuestionRepository.java
+++ reviewer/src/main/java/com/reviewer/reviewer/repositories/QuestionRepository.java
@@ -1,6 +1,7 @@
 package com.reviewer.reviewer.repositories;
 
 import com.reviewer.reviewer.models.Question;
+import com.reviewer.reviewer.models.QuestionForm;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
