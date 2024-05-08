c7f658d9cf989a2e87ee98f7c18991673b25e777
hora-keven <kevensantos993@gmail.com>
2024-05-08 14:10:19 -0300

    feat: question answer service almost finished

--- reviewer/src/main/java/com/reviewer/reviewer/services/QuestionAnswerService.java
+++ reviewer/src/main/java/com/reviewer/reviewer/services/QuestionAnswerService.java
@@ -1,12 +1,16 @@
 package com.reviewer.reviewer.services;
+
 import com.reviewer.reviewer.dto.questions.QuestionAnswerResponseDto;
 import com.reviewer.reviewer.dto.questions.QuestionResponseDto;
 import com.reviewer.reviewer.models.QuestionAnswer;
+import com.reviewer.reviewer.models.QuestionForm;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import com.reviewer.reviewer.repositories.QuestionRepository;
-import jakarta.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.reviewer.reviewer.dto.questions.QuestionAnswerDto;
@@ -15,10 +19,8 @@ import com.reviewer.reviewer.repositories.QuestionAnswerRepository;
 import com.reviewer.reviewer.repositories.QuestionFormRepository;
 import com.reviewer.reviewer.repositories.UserRepository;
 
-
 @Service
 public class QuestionAnswerService {
-
 
     @Autowired
     private QuestionAnswerRepository questionAnswerRepository;
@@ -32,29 +34,77 @@ public class QuestionAnswerService {
     @Autowired
     private QuestionRepository questionRepository;
 
+    // public List<QuestionAnswerResponseDto> create(QuestionAnswerDto data) {
+    //     Validation validation = new Validation(data);
+    //     var user = validation.UserNotFound(userRepository);
+    //     List<QuestionAnswer> answerResponseDtos = new ArrayList<>();
+
+    //     if (data.questionAnswer() == null || data.questionAnswer().isEmpty()) {
+    //         throw new IllegalArgumentException("The questionAnswer list is empty or null.");
+    //     }
+
+    //     for (var item : data.questionAnswer()) {
+    //         var questionId = item.question();
+    //         var answerText = item.answer().answer();
+
+    //         var question = questionRepository.findById(questionId)
+    //                 .orElseThrow(() -> new NoSuchElementException("Question not found!"));
+
+    //         // Obter o questionForm correspondente ao questionId
+    //         var questionForm = question.getQuestionForms();
+
+    //         // Verificar se há pelo menos um questionForm associado à pergunta
+    //         if (questionForm.isEmpty()) {
+    //             throw new NoSuchElementException("QuestionForm not found for question: " + questionId);
+    //         }
+
+    //         // Iterar sobre todos os questionForms associados à pergunta
+    //         for (var form : questionForm) {
+    //             System.out.println("QuestionForm: " + form.getId());
+    //             var questionAnswer = new QuestionAnswer(item, user, question, form);
+    //             questionAnswer.setAnswer(answerText);
+
+    //             answerResponseDtos.add(questionAnswer);
+    //             questionAnswerRepository.save(questionAnswer);
+    //         }
+    //     }
+
+    //     return QuestionAnswerResponseDto.fromQuestionAnswerList(answerResponseDtos);
+    // }
+
     public List<QuestionAnswerResponseDto> create(QuestionAnswerDto data) {
         Validation validation = new Validation(data);
-        var questionForm = validation.FormQuestionNotFound(questionFormRepository);
+        var questionForms = validation.QuestionFormNotFound(questionFormRepository);
         var user = validation.UserNotFound(userRepository);
-
         List<QuestionAnswer> answerResponseDtos = new ArrayList<>();
-
+        int i;
         if (data.questionAnswer() == null || data.questionAnswer().isEmpty()) {
             throw new IllegalArgumentException("The questionAnswer list is empty or null.");
+        }
+
+        // Itera sobre as perguntas do questionForm e imprime suas IDs
+        for (var question : questionForms) {
+            System.out.println("Question ID: " + question.getQuestion().getQuestionPt());
         }
 
         for (var item : data.questionAnswer()) {
             var questionId = item.question();
             var answerText = item.answer().answer();
-
-            System.out.println("Question ID: " + questionId);
-
             var question = questionRepository.findById(questionId)
-                    .orElseThrow(() -> new EntityNotFoundException("Question not found"));
+                    .orElseThrow(() -> new NoSuchElementException("Question not found!"));
 
+            // Obter o questionForm correspondente ao questionId
+            var questionForm = question.getQuestionForms();
+
+            // Verificar se há pelo menos um questionForm associado à pergunta
+            if (questionForm.isEmpty()) {
+                throw new NoSuchElementException("QuestionForm not found for question: " + questionId);
+            }
+
+            System.out.println("QuestionForm: " + questionForm.get(0).getForm().getId());
             System.out.println("Question do banco: " + question.getId());
 
-            var questionAnswer = new QuestionAnswer(item, user, question, questionForm);
+            var questionAnswer = new QuestionAnswer(item, user, question, questionForm.get(0));
             questionAnswer.setAnswer(answerText);
 
             answerResponseDtos.add(questionAnswer);
@@ -64,21 +114,21 @@ public class QuestionAnswerService {
         return QuestionAnswerResponseDto.fromQuestionAnswerList(answerResponseDtos);
     }
 
-//    public List<QuestionAnswerResponseDto> findAll(){
-//        var answersList = questionAnswerRepository.findAll();
-//        List<QuestionAnswerResponseDto> answersDto = new ArrayList<>();
-//        for (QuestionAnswer questionAnswer : answersList) {
-//            var answerDto = new QuestionAnswerResponseDto(questionAnswer);
-//            answersDto.add(answerDto);
-//        }
-//        return answersDto;
-//    }
+    // public List<QuestionAnswerResponseDto> findAll() {
+    //     var answersList = questionAnswerRepository.findAll();
+    //     List<QuestionAnswerResponseDto> answersDto = new ArrayList<>();
+    //     for (QuestionAnswer questionAnswer : answersList) {
+    //         var answerDto = new QuestionAnswerResponseDto(questionAnswer);
+    //         answersDto.add(answerDto);
+    //     }
+    //     return answersDto;
+    // }
 
-   public List<QuestionAnswerResponseDto> findByUserId(Long id){
+    public List<QuestionAnswerResponseDto> findByUserId(Long id) {
 
-       var answer = questionAnswerRepository.findByUserId(id);
+        var answer = questionAnswerRepository.findByUserId(id);
 
-       List<QuestionAnswerResponseDto> answersDto = new ArrayList<>();
+        List<QuestionAnswerResponseDto> answersDto = new ArrayList<>();
 
         for (QuestionAnswer questionAnswer : answer) {
             var question = new QuestionResponseDto(questionAnswer.getQuestion());
@@ -87,6 +137,6 @@ public class QuestionAnswerService {
         }
 
         return answersDto;
-   }
+    }
 
 }
