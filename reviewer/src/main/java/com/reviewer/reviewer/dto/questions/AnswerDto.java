11bb5f1f1a13b3ea8e525e3a9ae8f90911b653a2
hora-keven <kevensantos993@gmail.com>
2024-05-09 08:38:14 -0300

    fix: user id finished

--- reviewer/src/main/java/com/reviewer/reviewer/dto/questions/AnswerDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/questions/AnswerDto.java
@@ -3,8 +3,6 @@ package com.reviewer.reviewer.dto.questions;
 import jakarta.validation.constraints.NotNull;
 
 public record AnswerDto(
-        @NotNull
-        Long userId,
 
         @NotNull
         String answer
