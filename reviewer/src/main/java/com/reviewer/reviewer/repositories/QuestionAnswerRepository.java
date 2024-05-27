8e1549d54af3168a10d22b06d5323157fe9e4b4e
hora-keven <kevensantos993@gmail.com>
2024-05-27 09:49:20 -0300

    feat: new query created to find all by for which user id

--- reviewer/src/main/java/com/reviewer/reviewer/repositories/QuestionAnswerRepository.java
+++ reviewer/src/main/java/com/reviewer/reviewer/repositories/QuestionAnswerRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionAnswerRepository extends JpaRepository<QuestionAnswer, Long> {
-    List<QuestionAnswer> findByUserId(Long userId);
+    List<QuestionAnswer> findAllByForWhichUserId(Long userId);
     List<QuestionAnswer> findAllByQuestionId(Long questionId);
 }
