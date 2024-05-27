a3e9cbac2da434f75fc1ed1491a161beb4bb95fc
byankatomaz <byankatomaz@gmail.com>
2024-05-27 10:02:24 -0300

    fix: Modification to SSO

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormListDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormListDto.java
@@ -1,12 +1,11 @@
 package com.reviewer.reviewer.dto.forms;
 import com.reviewer.reviewer.dto.questions.QuestionResponseDto;
-import com.reviewer.reviewer.models.Question;
 
 import java.time.LocalDate;
 import java.util.List;
 
 
 
-public record QuestionFormListDto(Long id, String title, LocalDate year, List<QuestionResponseDto> questions) {
+public record QuestionFormListDto(Long id, List<IndicationFormResponseDto> indicated, String title, LocalDate year, List<QuestionResponseDto> questions) {
     
 }
