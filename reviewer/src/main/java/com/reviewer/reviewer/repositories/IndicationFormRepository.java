ee111874a728445fe57f1f02d197cb5c4bfffc5a
byankatomaz <byankatomaz@gmail.com>
2024-05-27 10:10:39 -0300

    fix: Modification to SSO

--- reviewer/src/main/java/com/reviewer/reviewer/repositories/IndicationFormRepository.java
+++ reviewer/src/main/java/com/reviewer/reviewer/repositories/IndicationFormRepository.java
@@ -3,5 +3,8 @@ package com.reviewer.reviewer.repositories;
 import com.reviewer.reviewer.models.IndicationForm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IndicationFormRepository extends JpaRepository<IndicationForm, Long> {
+    List<IndicationForm> findByUserIndicationId(Long indication);
 }
