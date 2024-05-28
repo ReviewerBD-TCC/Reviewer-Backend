5f0b053047ee2c4f2c487b70fe47c83eff792e9e
byankatomaz <byankatomaz@gmail.com>
2024-05-28 13:06:04 -0300

    fix: refactor code of UserDTO

--- reviewer/src/main/java/com/reviewer/reviewer/dto/users/UserResponseDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/users/UserResponseDto.java
@@ -1,11 +1,16 @@
 package com.reviewer.reviewer.dto.users;
 
+import com.auth0.jwt.JWT;
 import com.reviewer.reviewer.dto.users.Enums.TypeRole;
 import com.reviewer.reviewer.models.User;
+import org.springframework.security.oauth2.jwt.Jwt;
 
-public record UserResponseDto(Long id, String name, String email, String gkz, TypeRole type, String user, String manager) {
+import java.util.Map;
 
-    public UserResponseDto(User user) {
-        this(user.getId(), user.getName(),  user.getEmail(), user.getGkz(), user.getType(), user.getUser(), user.getManager());
+public record UserResponseDto(String id, String name, String mail) {
+
+    public UserResponseDto(Jwt jwtUser) {
+        this(jwtUser.getClaimAsString("id"), jwtUser.getClaimAsString("name"), jwtUser.getClaimAsString("mail"));
     }
+
 }
