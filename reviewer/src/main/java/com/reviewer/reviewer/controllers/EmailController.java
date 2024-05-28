f858ea9910983f39c9fda62846e63d0b139df63e
byankatomaz <byankatomaz@gmail.com>
2024-05-28 13:13:30 -0300

    fix: add compare roles

--- reviewer/src/main/java/com/reviewer/reviewer/controllers/EmailController.java
+++ reviewer/src/main/java/com/reviewer/reviewer/controllers/EmailController.java
@@ -1,16 +1,15 @@
 package com.reviewer.reviewer.controllers;
 
-
-
-
+import com.reviewer.reviewer.infra.functionsConfig.CompareRoles;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-
 import com.reviewer.reviewer.dto.email.EmailRecordDto;
 import com.reviewer.reviewer.services.EmailService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -22,9 +21,15 @@ public class EmailController {
 
 	@Autowired
 	private EmailService service;
-	
+
+	@Autowired
+	private CompareRoles roles;
+
 	@PostMapping
-	public ResponseEntity<?> sendEmail(@RequestBody EmailRecordDto data){
+	public ResponseEntity<?> sendEmail(@RequestBody EmailRecordDto data, @AuthenticationPrincipal Jwt tokenJWT){
+		if (!roles.compareRoles(tokenJWT).equals("ROLE_ADMIN")){
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
 		service.sendMail(data);
 		return ResponseEntity.status(HttpStatus.CREATED).body("Email sent successfully!");
 	}
