730814471032839f11a68c4d7167fcd4281e688d
hora-keven <kevensantos993@gmail.com>
2024-05-29 16:47:10 -0300

    fix: return form id

--- reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormListDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/forms/QuestionFormListDto.java
@@ -1,9 +1,7 @@
 package com.reviewer.reviewer.dto.forms;
 import com.reviewer.reviewer.dto.questions.QuestionResponseDto;
-import com.reviewer.reviewer.dto.users.RegisterResponseDto;
-import com.reviewer.reviewer.models.Question;
+import com.reviewer.reviewer.dto.users.UserDto;
 import com.reviewer.reviewer.models.QuestionForm;
-import com.reviewer.reviewer.models.User;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -11,10 +9,10 @@ import java.util.List;
 
 
 
-public record QuestionFormListDto(Long id, String title, LocalDate year, List<QuestionResponseDto> questions, RegisterResponseDto user) {
+public record QuestionFormListDto(Long id, String title, LocalDate year, List<QuestionResponseDto> questions, UserDto user) {
 
-    public QuestionFormListDto(QuestionForm questionForm, RegisterResponseDto user) {
-        this(questionForm.getId(), questionForm.getForm().getTitle(),questionForm.getForm().getYear(),convertToQuestionResponseDtos(questionForm.getForm().getQuestionForms()), user);
+    public QuestionFormListDto(QuestionForm questionForm, UserDto user) {
+        this(questionForm.getForm().getId(), questionForm.getForm().getTitle(),questionForm.getForm().getYear(),convertToQuestionResponseDtos(questionForm.getForm().getQuestionForms()), user);
     }
     private static List<QuestionResponseDto> convertToQuestionResponseDtos(List<QuestionForm> questionForms) {
         List<QuestionResponseDto> questions = new ArrayList<>();
