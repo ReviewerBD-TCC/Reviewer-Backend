2d69acc3dcf5d75fa0469fb133507c7c630c2415
hora-keven <kevensantos993@gmail.com>
2024-06-03 16:53:31 -0300

    feat: form title, quantity answered form and sent added  as a variable

--- reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionAnswerResponseDto.java
+++ reviewer/src/main/java/com/reviewer/reviewer/dto/questions/QuestionAnswerResponseDto.java
@@ -1,4 +1,5 @@
 package com.reviewer.reviewer.dto.questions;
+import com.reviewer.reviewer.models.Dashboard;
 import com.reviewer.reviewer.models.QuestionAnswer;
 
 import java.util.ArrayList;
@@ -7,6 +8,9 @@ import java.util.List;
 public record QuestionAnswerResponseDto(
         Long id,
         Long questionFormId,
+        String questionFormTitle,
+        Integer quantityAnsweredForm,
+        Integer quantityFormSent,
         String whoAnsweredId,
         String whoAnsweredName,
         QuestionResponseDto question,
@@ -15,16 +19,16 @@ public record QuestionAnswerResponseDto(
         String forWhichUser,
         String whichUserName
 ) {
-    public QuestionAnswerResponseDto(QuestionAnswer questionAnswer, QuestionResponseDto question) {
-        this(questionAnswer.getId(), questionAnswer.getQuestionForm().getForm().getId(), questionAnswer.getUser().getId(), questionAnswer.getUser().getName(), question, questionAnswer.getAnswer(), questionAnswer.getForWhichUser().getId(), questionAnswer.getForWhichUser().getName());
+    public QuestionAnswerResponseDto(QuestionAnswer questionAnswer, QuestionResponseDto question, Dashboard dashboard) {
+        this(questionAnswer.getId(), questionAnswer.getQuestionForm().getForm().getId(),questionAnswer.getQuestionForm().getForm().getTitle(), dashboard.getQuantityAnsweredFormSent(),dashboard.getFormQuantitySent(), questionAnswer.getUser().getId(), questionAnswer.getUser().getName(), question, questionAnswer.getAnswer(), questionAnswer.getForWhichUser().getId(), questionAnswer.getForWhichUser().getName());
     }
 
-    public static List<QuestionAnswerResponseDto> fromQuestionAnswerList(List<QuestionAnswer> answerList){
+    public static List<QuestionAnswerResponseDto> fromQuestionAnswerList(List<QuestionAnswer> answerList, Dashboard dashboard){
         List<QuestionAnswerResponseDto> dtos = new ArrayList<>();
 
         for(QuestionAnswer answer : answerList){
             var question = new QuestionResponseDto(answer.getQuestion());
-            dtos.add(new QuestionAnswerResponseDto(answer.getId(), answer.getQuestionForm().getForm().getId(), answer.getUser().getId(),answer.getUser().getName(), question, answer.getAnswer(), answer.getForWhichUser().getId(), answer.getForWhichUser().getName()));
+            dtos.add(new QuestionAnswerResponseDto(answer.getId(), answer.getQuestionForm().getForm().getId(),answer.getQuestionForm().getForm().getTitle(), dashboard.getQuantityAnsweredFormSent(),dashboard.getFormQuantitySent(), answer.getUser().getId(),answer.getUser().getName(), question, answer.getAnswer(), answer.getForWhichUser().getId(), answer.getForWhichUser().getName()));
         }
 
         return dtos;
