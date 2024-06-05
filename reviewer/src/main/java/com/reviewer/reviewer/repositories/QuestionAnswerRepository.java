7e1db1cb99784f100079c9b7e888f6e33d2aa007
hora-keven <kevensantos993@gmail.com>
2024-06-05 09:19:17 -0300

    feat: new query to seek users if has answers of forms

--- reviewer/src/main/java/com/reviewer/reviewer/repositories/QuestionAnswerRepository.java
+++ reviewer/src/main/java/com/reviewer/reviewer/repositories/QuestionAnswerRepository.java
@@ -5,6 +5,7 @@ import com.reviewer.reviewer.models.QuestionAnswer;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,4 +14,7 @@ public interface QuestionAnswerRepository extends JpaRepository<QuestionAnswer, 
     List<QuestionAnswer> findAllByUserId(String userId);
 
     List<QuestionAnswer> findAllByQuestionId(Long questionId);
+
+    @Query(value = "SELECT * FROM questions_answer  WHERE who_answered_id = ?1 and for_which_user_id = ?2", nativeQuery = true)
+    List<QuestionAnswer> findByUserIdAndForWhichUserId(String userId, String forWhichUserId);
 }
